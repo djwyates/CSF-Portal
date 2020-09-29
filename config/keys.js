ad4bf8a45a78367697be8f05a4f9c4363c53e5a1
David Yates <44711822+djwyates@users.noreply.github.com>
2020-09-29 12:58:38 -0700

    Create keys.js

---
+++ config/keys.js
@@ -0,0 +1,21 @@
+module.exports = {
+  accounts: {
+    admins: [],
+    domain: ""
+  },
+  aws: {
+    accessKeyId: "",
+    secretAccessKey: "",
+    region: ""
+  },
+  google: {
+    clientID: "",
+    clientSecret: ""
+  },
+  session: {
+    secret: ""
+  },
+  siteData: {
+    url: ""
+  }
+};
